5c84a1ea93ebb42f9e53040c69ab0ff5dc95019d
minhtoan1210 <nguyenvuminhtoan1999@gmail.com>
2021-12-24 18:15:41 +0700

    uploadtwo

--- js/main.js
+++ js/main.js
@@ -1,16 +1,34 @@
 $(document).ready(function () {
+    let slidershow = 4;
+    let auto = false;
     let header = $(".headerr").height();
     let height = header + (header/3);
-    console.log(header);
-    $(".page__home-header").css('marginTop',`${height}px`);
+    let slider = $(".slider");
+    let sliderwidth = slider.width();
+    if(sliderwidth<=987 && sliderwidth >= 764)
+    {
+      slidershow = 3;
+      auto=true;
+    }
+    else if(sliderwidth<=763 )
+    {
+      slidershow = 1;
+      auto=true;
+    }
+  //   let slidershow = slider[0].className;
+  //   let tablet = slidershow.search("tablet");
+  //  if(tablet !== -1 )
+  //  {
+  //   a = 3;
+  //  }
+    $(".page__home").css('marginTop',`${height}px`);
     $('.slider-content').slick(
         {
-          slidesToShow: 4,
+          slidesToShow: slidershow,
           slidesToScroll: 1,
-           //autoplay: true,
+          autoplay: auto,
           autoplaySpeed: 2000,
-         infinite: true,
-         centerMode: true,
+          infinite: true,
           prevArrow: `<div class="news__arrow news__arrow_dir_left">
           <img class="prev" src="https://img.icons8.com/ios-glyphs/30/000000/chevron-left.png"/>
           </div>`,
