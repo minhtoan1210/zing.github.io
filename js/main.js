5eb1fb60ed492dee233c93b1542b40f5ff9f6634
minhtoan1210 <nguyenvuminhtoan1999@gmail.com>
2022-01-12 20:16:24 +0700

    up load 50

--- js/main.js
+++ js/main.js
@@ -22,16 +22,10 @@ $(document).ready(function () {
   $(".slider-content").slick({
     slidesToShow: slidershow,
     slidesToScroll: 1,
-   // autoplay: auto,
+    autoplay: auto,
     centerMode: true,
     autoplaySpeed: 2000,
     infinite: true,
-    prevArrow: `<div class="news__arrow news__arrow_dir_left">
-          <img class="prev" src="https://img.icons8.com/ios-glyphs/30/000000/chevron-left.png"/>
-          </div>`,
-    nextArrow: `<div class="news__arrow news__arrow_dir_right">
-          <img class="next" src="https://img.icons8.com/ios-glyphs/30/000000/chevron-right.png"/>
-          </div>`,
   });
   $(".header__menu>ul>li.dots").click(function (e) {
     e.preventDefault();
